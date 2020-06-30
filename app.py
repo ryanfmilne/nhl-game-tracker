bcaa54caeb2fd3e3eabe7b49a43b1adb5dd50161
Ryan Milne <ryanmilne@MacBook-Pro.local>
2020-06-30 14:36:26 -0700

    Changed App Name and added a database

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nhltracker.db'
+db = SQLAlchemy(app)
+
+
+class Game(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	team = db.Column(db.String(100), nullable=False)
+	game_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+	def __repr__(self):
+		return 'game ' + str(self.id)
+
+
+db.create_all()
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/tracker')
+def tracker():
+	return render_template('tracker.html')
+
+
+ 
+if __name__ == "__main__":
+	app.run(debug=True)
