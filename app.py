d72146fc48a6380e2fb6b6d05c1b3b9a6f217b64
Ryan Milne <ryanmilne@MacBook-Pro.local>
2020-06-30 19:32:49 -0700

    Added database functionality

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -9,11 +9,24 @@ db = SQLAlchemy(app)
 
 class Game(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	team = db.Column(db.String(100), nullable=False)
+	away_team = db.Column(db.String(100), nullable=False)
+	away_player = db.Column(db.String(100), nullable=False)
+	away_period_1 = db.Column(db.Integer, nullable=False)
+	away_period_2 = db.Column(db.Integer, nullable=False)
+	away_period_3 = db.Column(db.Integer, nullable=False)
+	away_final = db.Column(db.Integer, nullable=False)
+	home_team = db.Column(db.String(100), nullable=False)
+	home_player = db.Column(db.String(100), nullable=False)
+	home_period_1 = db.Column(db.Integer, nullable=False)
+	home_period_2 = db.Column(db.Integer, nullable=False)
+	home_period_3 = db.Column(db.Integer, nullable=False)
+	home_final = db.Column(db.Integer, nullable=False)
+
 	game_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 	def __repr__(self):
 		return 'game ' + str(self.id)
+
 
 
 class Team(db.Model):
@@ -23,6 +36,8 @@ class Team(db.Model):
 	def __repr__(self):
 		return 'team ' + str(self.id)
 		
+
+
 class Player(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	name = db.Column(db.String(100), nullable=False)
@@ -35,9 +50,39 @@ class Player(db.Model):
 def index():
 	return render_template('index.html')
 
-@app.route('/tracker')
+@app.route('/tracker', methods=['GET', 'POST'])
 def tracker():
-	return render_template('tracker.html')
+
+	if request.method == 'POST':
+		away_1 = request.form['away-period-1']
+		away_2 = request.form['away-period-2']
+		away_3 = request.form['away-period-3']
+		home_1 = request.form['home-period-1']
+		home_2 = request.form['home-period-2']
+		home_3 = request.form['home-period-3']
+		away_final_score = int(away_1) + int(away_2) + int(away_3)
+		home_final_score = int(home_1) + int(home_2) + int(home_3)
+
+		all_games = Game(
+			away_team='Nashville Predators',
+			away_player='Ryan',
+			away_period_1=away_1,
+			away_period_2=away_2,
+			away_period_3=away_3,
+			away_final=away_final_score, #
+			home_team='Washington Capitals',
+			home_player='Nick',
+			home_period_1=home_1,
+			home_period_2=home_2,
+			home_period_3=home_3,
+			home_final=home_final_score,
+			)
+		db.session.add(all_games)
+		db.session.commit()
+		return redirect('/tracker')
+	else:
+		all_games = Game.query.order_by(Game.game_date).all()	
+		return render_template('tracker.html', games=all_games)
 
 
  
